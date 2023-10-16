accbfe1b34388b333ccee570661cf3fd6ddfce31
WELLINGTON FERREIRA BARBOSA <wellington.barbosa@pcce.intranet>
2023-10-16 16:46:40 -0300

    Second Day

---
+++ Python/secao2/python/aula1 - (print-coment-doctring).py
@@ -0,0 +1,15 @@
+"""
+Docstring
+
+Aprendo o suficiente... Aprendo muito
+
+O código abaixo printa os números
+"""
+
+
+
+
+# Permite escrever um comentário
+print(73) # Na frente
+#abaixo
+print(123)
